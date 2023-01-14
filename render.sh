c20ce6cb2d71747f398c1962503beb60abd1f01d
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2023-01-14 15:11:54 +0100

    Add comment

--- render.sh
+++ render.sh
@@ -6,5 +6,7 @@ quarto render --to docx &&
 	cd _out &&
 	# soffice Facharbeit.docx &&
 	soffice --headless --convert-to pdf Facharbeit.docx &&
+	# evince stops watching a file if it gets deleted
+	# so we can preview this copy which will never be deleted
 	cp ./Facharbeit.pdf ../_preview/Facharbeit.pdf -f #&&
 # evince Facharbeit.pdf
