4c9776039aa3940f6329969bfaeb4f40c3d1c5f7
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2023-01-16 17:25:58 +0100

    16.01.2023

--- watch.sh
+++ watch.sh
@@ -1,1 +1,1 @@
-exa --git-ignore --no-filesize --no-time --no-permissions --no-user custom/reference-doc.docx *.yml *.qmd chapters/*.qmd | entr ./render.sh
+exa --git-ignore --no-filesize --no-time --no-permissions --no-user custom/reference-doc.docx _quarto.yml cite/*.bib index.qmd chapters/*.qmd | entr ./render.sh
