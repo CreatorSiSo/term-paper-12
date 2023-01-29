a19acd807b4989a44f09ac3ae0a401d56caa7046
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2023-01-29 13:40:12 +0100

    29.01.2023

--- render.sh
+++ render.sh
@@ -8,5 +8,4 @@ quarto render --to docx --cache &&
 	soffice --headless --convert-to pdf Facharbeit.docx &&
 	# evince stops watching a file if it gets deleted
 	# so we can preview this copy which will never be deleted
-	cp ./Facharbeit.pdf ../_preview/Facharbeit.pdf -f #&&
-# evince Facharbeit.pdf
+	cp ./Facharbeit.pdf ../_preview/Facharbeit.pdf -f
--- watch.sh
+++ watch.sh
@@ -1,1 +1,2 @@
+evince _preview/Facharbeit.pdf &
 exa --git-ignore --no-filesize --no-time --no-permissions --no-user custom/reference-doc.docx _quarto.yml cite/*.bib index.qmd chapters/*.qmd | entr ./render.sh
